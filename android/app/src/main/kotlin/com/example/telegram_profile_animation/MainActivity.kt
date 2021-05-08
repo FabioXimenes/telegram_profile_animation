3810f1dde1b6be860cd526b1412b1f2d92bbea71
FabioXimenes <fabio.ximenes@lapisco.ifce.edu.br>
2021-05-07 22:08:44 -0300

    Add first implementation

---
+++ android/app/src/main/kotlin/com/example/telegram_profile_animation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.telegram_profile_animation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/my_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:telegram_profile_animation/app/profile_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home: ProfilePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
